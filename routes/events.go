69ce08a075e61f213e5189f52f8cd6ad2560c0ee
Rumee Ahmed <rumeeahmad@gmail.com>
2024-11-11 16:10:32 +0000

    auth

--- routes/events.go
+++ routes/events.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"go-events-booking-api/models"
+	"go-events-booking-api/utils"
 	rand2 "math/rand"
 	"net/http"
 	"strconv"
@@ -35,8 +36,21 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusUnprocessableEntity, gin.H{"message": err.Error()})
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
@@ -13,4 +15,34 @@ func GenerateToken(email string, userId int64) (string, error) {
 		jwt.MapClaims{"email": email, "user_id": userId, "exp": time.Now().Add(time.Hour * 1).Unix()},
 	)
 	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+
+		// This checks if the signing method is a type of SigningMethodHMAC.
+		// If its not ok, then execute the code in the if body.
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println(ok)
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return errors.New("invalid token")
+	}
+
+	//claims, ok := token.Claims.(jwt.MapClaims)
+	//if !ok {
+	//	return errors.New("invalid claims")
+	//}
+
+	//email := claims["email"].(string)
+	//userId := claims["user_id"].(int64)
+
+	return nil
 }
